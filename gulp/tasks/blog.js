1423c3f3305e13da7b7ef12996bf1e65582afca5
belgac <hans.scheuren@bizliner.com>
2015-03-24 17:29:26 +0000

    corrected grouped blog task

---
+++ gulp/tasks/blog.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
+gulp.task('blog', function(callback) {
+  runSequence(
+    'posts',
+    ['index', 'archive', 'tags', 'rss'],
+    callback
+  )
+});
