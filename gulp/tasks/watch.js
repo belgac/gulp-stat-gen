b0125ecd0d8be750c7248e561b594650cf17c022
belgac <hans.scheuren@bizliner.com>
2015-03-24 17:12:44 +0000

    fixed typo in watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,7 +4,7 @@ var config = require('../config').watch;
 
 gulp.task('watch', ['default','browsersync'], function () {
   gulp.watch(config.assetsSrc, ['assets']);
-  gulp.watch(config.mediaSrc, ['media'])
+  gulp.watch(config.mediaSrc, ['media']);
   gulp.watch(config.pagesSrc, ['pages']);
   gulp.watch(config.blogSrc, ['posts', 'index', 'archive', 'tags', 'rss']);
   gulp.watch(config.partialsSrc, ['pages','posts', 'index', 'archive', 'tags', 'rss']);
