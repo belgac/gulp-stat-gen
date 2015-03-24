6168bde4613e15b4094b9ccb9877bd37cfb5e681
belgac <hans.scheuren@bizliner.com>
2015-03-24 16:31:58 +0000

    corrected merge errors

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,20 +1,11 @@
 var gulp = require('gulp');
-var connect = require('connect');
-var http = require('http');
 
 var config = require('../config').watch;
 
-gulp.task('watch', ['default'], function () {
+gulp.task('watch', ['default','browsersync'], function () {
   gulp.watch(config.assetsSrc, ['assets']);
   gulp.watch(config.mediaSrc, ['media'])
   gulp.watch(config.pagesSrc, ['pages']);
   gulp.watch(config.blogSrc, ['posts', 'index', 'archive', 'tags', 'rss']);
   gulp.watch(config.partialsSrc, ['pages','posts', 'index', 'archive', 'tags', 'rss']);
-  
-  
-  var app = connect()
-    .use(connect.static('build'))
-    .use(connect.directory('build'));
-  
-  http.createServer(app).listen(3000);
 });
