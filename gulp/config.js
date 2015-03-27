6a92971b803c94fdc0ef3ff886281c636593b7c3
belgac <hans.scheuren@bizliner.com>
2015-03-27 14:14:49 +0000

    implemented template inheritance

--- gulp/config.js
+++ gulp/config.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   prepareTemplate:{
     src:bowerDir+"/"+siteConfig.theme+"/**/*",
+    bowerDir: bowerDir,
     dest:baseTheme+"/"
   },
   bower:{
--- gulp/tasks/prepare-template.js
+++ gulp/tasks/prepare-template.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
-
+var mainBowerFiles = require('main-bower-files');
+var rename = require('gulp-rename')
 var config = require('../config').prepareTemplate;
 
 gulp.task('prepareTemplate', function() {
-      return gulp.src(config.src)        
+      return gulp.src(mainBowerFiles(), { base: config.bowerDir }) 
+        .pipe(rename(function (path) {
+          path.dirname = path.dirname.substring(path.dirname.indexOf('/')+1);
+        }))
         .pipe(gulp.dest(config.dest));
 });
